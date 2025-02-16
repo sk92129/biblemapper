82e4e6c7840c50d09ef2cdcdc6823682b6954066
Sean K <1682772+sk92129@users.noreply.github.com>
2025-02-15 21:32:13 -0800

    add home nav text

--- lib/screens/widgets/appframe_custom.dart
+++ lib/screens/widgets/appframe_custom.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AppFrameWidget extends StatefulWidget {
   const AppFrameWidget(
