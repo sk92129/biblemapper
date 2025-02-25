7cc1d12cd224b2f09edca9727d1fdeeab7e3d60b
Sean K <1682772+sk92129@users.noreply.github.com>
2025-02-24 17:22:48 -0800

    semantics

--- lib/initial_services.dart
+++ lib/initial_services.dart
@@ -1,2 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+
 initializeServices() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // when this semantics binding is initialized, it caused issues with the dropdown list
+  
+  SemanticsBinding.instance.ensureSemantics();
 }
