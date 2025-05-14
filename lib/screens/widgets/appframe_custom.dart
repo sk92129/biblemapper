28e48be33f750c46857db4b979e5f3eb9a5975b0
Sean K <1682772+sk92129@users.noreply.github.com>
2025-05-14 10:12:40 -0700

    add hamburger menu to the top scaffold.

--- lib/screens/widgets/appframe_custom.dart
+++ lib/screens/widgets/appframe_custom.dart
@@ -23,6 +23,70 @@ class _AppFrameWidgetState extends State<AppFrameWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              child: Text(
+                AppLocalizations.of(context)!.appTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: Text(AppLocalizations.of(context)!.homeNav),
+              selected: currentPageIndex == 0,
+              onTap: () {
+                setState(() {
+                  currentPageIndex = 0;
+                });
+                context.goNamed(_pages[0]);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.calendar_today),
+              title: Text(AppLocalizations.of(context)!.biblePlansNav),
+              selected: currentPageIndex == 1,
+              onTap: () {
+                setState(() {
+                  currentPageIndex = 1;
+                });
+                context.goNamed(_pages[1]);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.people),
+              title: Text(AppLocalizations.of(context)!.communityNav),
+              selected: currentPageIndex == 2,
+              onTap: () {
+                setState(() {
+                  currentPageIndex = 2;
+                });
+                context.goNamed(_pages[2]);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: Text(AppLocalizations.of(context)!.profileNav),
+              selected: currentPageIndex == 3,
+              onTap: () {
+                setState(() {
+                  currentPageIndex = 3;
+                });
+                context.goNamed(_pages[3]);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
           setState(() {
@@ -57,9 +121,7 @@ class _AppFrameWidgetState extends State<AppFrameWidget> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(AppLocalizations.of(context)!.appTitle),
       ),
-
-        body: widget.child,
-
+      body: widget.child,
     );
   }
 }
